73092e6dec817fe0ff1da44eeae48b691408ef43
Roman Shtylman <roman@foxglove.dev>
2021-12-02 16:13:17 -0800

    Add bag2proto script to typescript folder (#3)
    Convert a ros1 bag file to a protobuf mcap file.

---
+++ typescript/scripts/bag2proto.ts
@@ -0,0 +1,226 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/
+
+// convert a ROS1 .bag file to an mcap proto file
+
+import { Bag } from "@foxglove/rosbag";
+import { FileReader } from "@foxglove/rosbag/node";
+import { parse as parseMessageDefinition } from "@foxglove/rosmsg";
+import { program } from "commander";
+import protobufjs from "protobufjs";
+import descriptor from "protobufjs/ext/descriptor";
+import decompressLZ4 from "wasm-lz4";
+
+import { McapWriter, ChannelInfo, Message } from "../src";
+
+const builtinSrc = `
+syntax = "proto3";
+
+package ros;
+
+message Time {
+  fixed32 sec = 1;
+  fixed32 nsec = 2;
+}
+
+message Duration {
+    fixed32 sec = 1;
+    fixed32 nsec = 2;
+  }
+`;
+
+const BUILTIN_TYPE_MAP = new Map([
+  ["time", "ros.Time"],
+  ["duration", "ros.Duration"],
+  ["uint8", "int32"],
+  ["uint16", "int32"],
+  ["int8", "sint32"],
+  ["int16", "sint32"],
+  ["float32", "float"],
+  ["float64", "double"],
+]);
+
+function rosTypenameToProtoPath(typeName: string): string {
+  return `ros.${typeName.replace("/", ".")}`;
+}
+
+// convert a combined ros string message definition to protobuf Root instance
+function rosMsgDefinitionToProto(typeName: string, msgDef: string): protobufjs.Root {
+  const definitionArr = parseMessageDefinition(msgDef);
+  const root = new protobufjs.Root();
+
+  const BuiltinSrcParse = protobufjs.parse(builtinSrc, { keepCase: true });
+  root.add(BuiltinSrcParse.root);
+
+  for (const def of definitionArr) {
+    const rosDatatypeName = def.name ?? typeName;
+    const nameParts = rosDatatypeName.split("/");
+    if (nameParts.length !== 2) {
+      throw new Error(`Invalid name ${typeName}`);
+    }
+    const packageName = nameParts[0]!;
+    const msgName = nameParts[1]!;
+
+    const fields: string[] = [];
+    let fieldNumber = 1;
+    for (const field of def.definitions) {
+      if (field.isConstant === true) {
+        // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+        fields.push(`// ${field.type} ${field.name} = ${field.valueText ?? field.value ?? ""}`);
+        continue;
+      }
+      const lineComments: string[] = [];
+      const qualifiers = [];
+      if (field.type === "uint8" && field.isArray === true) {
+        qualifiers.push("bytes");
+      } else {
+        if (field.isArray === true) {
+          qualifiers.push("repeated");
+        }
+        if (field.isComplex === true) {
+          qualifiers.push(rosTypenameToProtoPath(field.type));
+        } else if (BUILTIN_TYPE_MAP.has(field.type)) {
+          const protoType = BUILTIN_TYPE_MAP.get(field.type)!;
+          if (protoType.includes("int")) {
+            lineComments.push(`originally ${field.type}`);
+          }
+          qualifiers.push(BUILTIN_TYPE_MAP.get(field.type)!);
+        } else {
+          qualifiers.push(field.type);
+        }
+      }
+      if (field.arrayLength != undefined) {
+        lineComments.push(`length ${field.arrayLength}`);
+      }
+      fields.push(
+        `${qualifiers.join(" ")} ${field.name} = ${fieldNumber++};${
+          lineComments.length > 0 ? " // " + lineComments.join(", ") : ""
+        }`,
+      );
+    }
+
+    const outputSections = [
+      `// Generated from ${rosDatatypeName}`,
+
+      'syntax = "proto3";',
+
+      `package ros.${packageName};`,
+
+      `message ${msgName} {\n  ${fields.join("\n  ")}\n}`,
+    ];
+
+    const protoSrc = outputSections.filter(Boolean).join("\n\n") + "\n";
+
+    const ProtoSrcParse = protobufjs.parse(protoSrc, { keepCase: true });
+    root.add(ProtoSrcParse.root);
+  }
+
+  return root;
+}
+
+type TopicDetail = {
+  channelInfo: ChannelInfo;
+  MsgRoot: protobufjs.Type;
+};
+
+async function convert(filePath: string) {
+  await decompressLZ4.isLoaded;
+
+  const bag = new Bag(new FileReader(filePath));
+  await bag.open();
+
+  const mcapFilePath = filePath.replace(".bag", ".mcap");
+  console.debug(`Writing to ${mcapFilePath}`);
+
+  const mcapFile = new McapWriter();
+  await mcapFile.open(mcapFilePath);
+
+  const topicToDetailMap = new Map<string, TopicDetail>();
+
+  for (const [, connection] of bag.connections) {
+    if (!connection.type) {
+      continue;
+    }
+
+    const schemaName = rosTypenameToProtoPath(connection.type);
+
+    const root = rosMsgDefinitionToProto(connection.type, connection.messageDefinition);
+    const MsgRoot = root.lookupType(schemaName);
+
+    // create a descriptor message for the root
+    // Strip leading `.` from the package names to make them relative to the descriptor
+    const descriptorMsg = root.toDescriptor("proto3");
+    for (const desc of descriptorMsg.file) {
+      desc.package = desc.package?.substring(1);
+    }
+
+    const descriptorMsgEncoded = descriptor.FileDescriptorSet.encode(descriptorMsg).finish();
+
+    const channelInfo: ChannelInfo = {
+      type: "ChannelInfo",
+      id: topicToDetailMap.size,
+      topic: connection.topic,
+      encoding: "protobuf",
+      schemaName,
+      schema: protobufjs.util.base64.encode(
+        descriptorMsgEncoded,
+        0,
+        descriptorMsgEncoded.byteLength,
+      ),
+      data: new ArrayBuffer(0),
+    };
+
+    topicToDetailMap.set(connection.topic, {
+      channelInfo,
+      MsgRoot,
+    });
+    await mcapFile.write(channelInfo);
+  }
+
+  const mcapMessages: Array<Message> = [];
+  await bag.readMessages(
+    {
+      decompress: {
+        lz4: (buffer: Uint8Array, size: number) => decompressLZ4(buffer, size),
+      },
+    },
+    (result) => {
+      const detail = topicToDetailMap.get(result.topic);
+      if (!detail) {
+        return;
+      }
+
+      const { channelInfo, MsgRoot } = detail;
+
+      const protoMsg = MsgRoot.fromObject(result.message as Record<string, unknown>);
+      const protoMsgBuffer = MsgRoot.encode(protoMsg).finish();
+
+      const timestamp = BigInt(result.timestamp.sec) * 1000000000n + BigInt(result.timestamp.nsec);
+      const msg: Message = {
+        type: "Message",
+        channelInfo,
+        timestamp,
+        data: protoMsgBuffer,
+      };
+
+      mcapMessages.push(msg);
+    },
+  );
+
+  for (const msg of mcapMessages) {
+    await mcapFile.write(msg);
+  }
+
+  await mcapFile.end();
+}
+
+program
+  .argument("<file...>", "path to .bag file(s)")
+  .description("Convert a ROS1 .bag file to a mcap file with protobuf messages")
+  .action(async (files: string[]) => {
+    for (const file of files) {
+      await convert(file).catch(console.error);
+    }
+  })
+  .parse();
