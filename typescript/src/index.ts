e9250ff2130a5f28a59cdd625dd1a7f9388a4302
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-01-20 14:43:41 -0800

    typescript: Export detectVersion and McapPre0To0StreamReader (#31)

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,7 +1,10 @@
 export { default as McapPre0Reader } from "./pre0/McapPre0Reader";
 export { default as McapPre0Writer } from "./pre0/McapPre0Writer";
+export { default as McapPre0To0StreamReader } from "./pre0/McapPre0To0StreamReader";
 export * as McapPre0Types from "./pre0/types";
 
 export { default as Mcap0IndexedReader } from "./v0/Mcap0IndexedReader";
 export { default as Mcap0StreamReader } from "./v0/Mcap0StreamReader";
 export * as Mcap0Types from "./v0/types";
+
+export * from "./common/detectVersion";
