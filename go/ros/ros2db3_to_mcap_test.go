644e0fe808edbcde466cc757dcc63f8bfd45bcf5
Wyatt Alt <wyatt.alt@gmail.com>
2022-01-28 12:59:46 -0800

    Add support for conversion of ros2 db3 bagfiles (#77)
    Adds support to the mcap CLI tool for converting ros2 db3 files to mcap.
    This should work without arguments on a system where AMENT_PREFIX_PATH
    points to /opt/ros/<distro>, which seems to be the default ROS install.
    Otherwise the equivalent search directories must be supplied via
    parameter.

---
+++ go/ros/ros2db3_to_mcap_test.go
@@ -0,0 +1,30 @@
+package ros
+
+import (
+	"bytes"
+	"database/sql"
+	"testing"
+
+	"github.com/foxglove/mcap/go/libmcap"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestDB3MCAPConversion(t *testing.T) {
+	db3file := "../../testdata/db3/chatter.db3"
+	buf := &bytes.Buffer{}
+	db, err := sql.Open("sqlite3", db3file)
+	assert.Nil(t, err)
+
+	err = DB3ToMCAP(db, buf, []string{"./testdata/galactic"})
+	assert.Nil(t, err)
+
+	reader, err := libmcap.NewReader(bytes.NewReader(buf.Bytes()))
+	assert.Nil(t, err)
+
+	info, err := reader.Info()
+	assert.Nil(t, err)
+	assert.Equal(t, uint64(7), info.Statistics.MessageCount)
+	assert.Equal(t, 1, len(info.Channels))
+	assert.Equal(t, "/chatter", info.Channels[1].TopicName)
+}
