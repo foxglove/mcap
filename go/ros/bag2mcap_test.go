6e619bbb3a694330a0d742150bfd065d44fcdedb
Wyatt Alt <wyatt.alt@gmail.com>
2022-02-02 14:01:43 -0800

    Minor allocation reductions for go bag transcoder
    Adds a basic benchmark test for the mcap to bag conversion, and reuses a
    message buffer across messages when transcoding.

---
+++ go/ros/bag2mcap_test.go
@@ -0,0 +1,47 @@
+package ros
+
+import (
+	"bytes"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func BenchmarkBag2MCAP(b *testing.B) {
+	cases := []struct {
+		assertion string
+		inputfile string
+	}{
+		{
+			"demo bag",
+			"../../testdata/bags/demo.bag",
+		},
+		// {
+		// 	"cal loop",
+		// 	"~/data/bags/cal_loop.bag",
+		// },
+	}
+	for _, c := range cases {
+		stats, err := os.Stat(c.inputfile)
+		assert.Nil(b, err)
+		input, err := os.ReadFile(c.inputfile)
+		assert.Nil(b, err)
+		reader := &bytes.Reader{}
+		writer := &bytes.Buffer{}
+		b.ResetTimer()
+		b.Run(c.assertion, func(b *testing.B) {
+			for n := 0; n < b.N; n++ {
+				t0 := time.Now()
+				reader.Reset(input)
+				writer.Reset()
+				err = Bag2MCAP(reader, writer)
+				assert.Nil(b, err)
+				elapsed := time.Since(t0)
+				mbread := stats.Size() / (1024 * 1024)
+				b.ReportMetric(float64(mbread)/elapsed.Seconds(), "MB/sec")
+			}
+		})
+	}
+}
