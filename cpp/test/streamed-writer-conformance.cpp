471d8d5dd2a4b2c8c610d05b0b9c0470c1b9c26f
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2022-02-16 13:14:01 -0800

    enable CRCs in C++ writer conformance runner

--- cpp/test/streamed-writer-conformance.cpp
+++ cpp/test/streamed-writer-conformance.cpp
@@ -21,7 +21,6 @@ mcap::McapWriterOptions ReadOptions(const json& featuresJson) {
   // ["ch", "mx", "st", "rsh", "rch", "chx", "sum", "pad"]
   mcap::McapWriterOptions options{""};
   options.compression = mcap::Compression::None;
-  options.noCRC = true;
   options.noChunking = true;
   options.noMessageIndex = true;
   options.noSummary = true;
