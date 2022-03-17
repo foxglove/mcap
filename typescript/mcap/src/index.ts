0214707d67b53b7220938b7edbb7528d1ec4a690
Roman Shtylman <roman@foxglove.dev>
2022-03-16 20:51:10 -0700

    Add typescript example bag2mcap
    bag2mcap converts a bag file to an mcap file with protobuf schema and message encoding.

--- typescript/mcap/src/index.ts
+++ typescript/mcap/src/index.ts
@@ -10,5 +10,6 @@ export { Mcap0RecordBuilder } from "./v0/Mcap0RecordBuilder";
 export { ChunkBuilder as Mcap0ChunkBuilder } from "./v0/ChunkBuilder";
 export * as Mcap0Types from "./v0/types";
 export * as Mcap0Constants from "./v0/constants";
+export type { IWritable } from "./common/IWritable";
 
 export * from "./common/detectVersion";
