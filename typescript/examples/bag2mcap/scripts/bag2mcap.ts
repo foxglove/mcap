f31343d19aa3c0d0507b85c2ae153487e42e39f6
Roman Shtylman <roman@foxglove.dev>
2022-03-22 09:06:27 -0700

    Add bag2mcap example to CI

--- typescript/examples/bag2mcap/scripts/bag2mcap.ts
+++ typescript/examples/bag2mcap/scripts/bag2mcap.ts
@@ -9,6 +9,7 @@ import { Bag } from "@foxglove/rosbag";
 import { FileReader } from "@foxglove/rosbag/node";
 import { parse as parseMessageDefinition } from "@foxglove/rosmsg";
 import { Time } from "@foxglove/rosmsg-serialization";
+import { toNanoSec } from "@foxglove/rostime";
 import Bzip2 from "@foxglove/wasm-bz2";
 import { program } from "commander";
 import { open, FileHandle } from "fs/promises";
