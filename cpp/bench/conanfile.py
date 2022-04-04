2a41f5e81cc30808f76f1eccf1b0ea8f2a30a8dc
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2022-04-04 13:53:48 -0700

    cpp: bump version to 0.1.1 (#360)
    **Public-Facing Changes**
    None

--- cpp/bench/conanfile.py
+++ cpp/bench/conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake
 class McapBenchmarksConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    requires = "benchmark/1.6.0", "mcap/0.0.1"
+    requires = "benchmark/1.6.0", "mcap/0.1.1"
 
     def build(self):
         cmake = CMake(self)
--- cpp/examples/conanfile.py
+++ cpp/examples/conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake
 class McapExamplesConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    requires = "mcap/0.0.1"
+    requires = "mcap/0.1.1"
 
     def build(self):
         cmake = CMake(self)
