fd99b5cd99fc52db2ce3c3c7e849009c9c016ef7
James Smith <james@foxglove.dev>
2022-07-12 15:27:03 +1000

    cleanup

--- cpp/examples/protobuf_pointcloud_writer/protobuf_pointcloud_writer.cpp
+++ cpp/examples/protobuf_pointcloud_writer/protobuf_pointcloud_writer.cpp
@@ -14,12 +14,14 @@
 #define POINTS_PER_CLOUD 1000
 #define FIELDS_PER_POINT 3
 
+// Point represents a point in 3D space.
 struct Point {
   float x;
   float y;
   float z;
 };
-// Utility class to generate random points on a sphere on demand.
+
+// PointGenerator generates random points on a sphere.
 class PointGenerator {
 private:
   std::mt19937 _generator;
@@ -30,6 +32,7 @@ public:
       : _generator(seed)
       , _distribution(0.0, 1.0) {}
 
+  // next produces a random point on the unit sphere, scaled by `scale`.
   Point next(float scale) {
     float theta = 2 * M_PI * _distribution(_generator);
     float phi = acos(1 - 2 * _distribution(_generator));
@@ -37,35 +40,40 @@ public:
     point.x = float((sin(phi) * cos(theta)) * scale);
     point.y = float((sin(phi) * sin(theta)) * scale);
     point.z = float(cos(phi) * scale);
-    // std::cout << "x: " << point.x << " y: " << point.y << " z: " << point.z << std::endl;
     return point;
   }
 };
 
-mcap::Timestamp now() {
-  return mcap::Timestamp(std::chrono::duration_cast<std::chrono::nanoseconds>(
-                           std::chrono::system_clock::now().time_since_epoch())
-                           .count());
-}
-
-void write_float_little_endian(float input, std::string* output, size_t offset) {
+// WriteLittleEndianFloat writes a little-endian float into the `output` string of bytes at
+// `offset`.
+void WriteLittleEndianFloat(float input, std::string* output, size_t offset) {
   static_assert(sizeof(uint32_t) == sizeof(float));
-  uint32_t as_int = *reinterpret_cast<uint32_t*>(&input);
-  // little-endian means the LSB gets encoded first.
-  assert(output->size() > offset + 3);
-  (*output)[offset + 0] = as_int & 0xFF;
-  (*output)[offset + 1] = (as_int >> 8) & 0xFF;
-  (*output)[offset + 2] = (as_int >> 16) & 0xFF;
-  (*output)[offset + 3] = (as_int >> 24) & 0xFF;
+  uint32_t asInt = *reinterpret_cast<uint32_t*>(&input);
+  if (output->size() <= offset + 3) {
+    std::cerr << "tried to write 4 bytes at offset " << offset << " into buffer of size "
+              << output->size() << std::endl;
+    abort();
+  }
+  // write in little-endian order, no matter what endian-ness the host uses.
+  // the LSB goes into the first byte.
+  (*output)[offset + 0] = asInt & 0xFF;
+  (*output)[offset + 1] = (asInt >> 8) & 0xFF;
+  (*output)[offset + 2] = (asInt >> 16) & 0xFF;
+  (*output)[offset + 3] = (asInt >> 24) & 0xFF;
 }
 
-int main(int, char**) {
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <output.mcap>\n";
+    return 1;
+  }
+  const char* outputFilename = argv[1];
+
   mcap::McapWriter writer;
   auto options = mcap::McapWriterOptions("x-protobuf");
-  const char OutputFilename[] = "sphere.mcap";
-  std::ofstream out(OutputFilename, std::ios::binary);
+  std::ofstream out(outputFilename, std::ios::binary);
   writer.open(out, options);
-
+  // set up the schema and channel.
   mcap::Schema schema("foxglove.PointCloud", "protobuf",
                       std::string_view((char*)(descriptor_pb_bin), descriptor_pb_bin_len));
   writer.addSchema(schema);
@@ -73,11 +81,11 @@ int main(int, char**) {
   mcap::Channel channel("/pointcloud", "protobuf", schema.id);
   writer.addChannel(channel);
 
-  mcap::Timestamp start_time = std::chrono::duration_cast<std::chrono::nanoseconds>(
-                                 std::chrono::system_clock::now().time_since_epoch())
-                                 .count();
+  mcap::Timestamp startTime = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                                std::chrono::system_clock::now().time_since_epoch())
+                                .count();
 
-  PointGenerator point_gen;
+  PointGenerator pointGenerator;
   foxglove::PointCloud pcl;
   foxglove::Pose* pose = pcl.mutable_pose();
   foxglove::Vector3* position = pose->mutable_position();
@@ -90,45 +98,44 @@ int main(int, char**) {
   orientation->set_z(0);
   orientation->set_w(1);
 
-  pcl.set_point_stride(12);
-  auto field_x = pcl.add_fields();
-  field_x->set_name("x");
-  field_x->set_offset(0);
-  field_x->set_type(foxglove::PackedElementField_NumericType_FLOAT32);
-  auto field_y = pcl.add_fields();
-  field_y->set_name("y");
-  field_y->set_offset(1 * sizeof(float));
-  field_y->set_type(foxglove::PackedElementField_NumericType_FLOAT32);
-  auto field_z = pcl.add_fields();
-  field_z->set_name("z");
-  field_z->set_offset(2 * sizeof(float));
-  field_z->set_type(foxglove::PackedElementField_NumericType_FLOAT32);
-
+  pcl.set_point_stride(sizeof(float) * FIELDS_PER_POINT);
+  const char* const fieldNames[] = {"x", "y", "z"};
+  int fieldOffset = 0;
+  for (const auto& name : fieldNames) {
+    auto field = pcl.add_fields();
+    field->set_name(name);
+    field->set_offset(fieldOffset);
+    field->set_type(foxglove::PackedElementField_NumericType_FLOAT32);
+    fieldOffset += sizeof(float);
+  }
   pcl.mutable_data()->append(POINTS_PER_CLOUD * FIELDS_PER_POINT * sizeof(float), '\0');
-  pcl.set_frame_id("/pointcloud");
+  pcl.set_frame_id("pointcloud");
 
-  for (uint64_t frame_index = 0; frame_index < 100; ++frame_index) {
-    mcap::Timestamp cloud_time = start_time + (frame_index * 100 * NS_PER_MS);
+  // write 100 pointcloud messages into the output MCAP file.
+  for (uint64_t frameIndex = 0; frameIndex < 100; ++frameIndex) {
+    mcap::Timestamp cloudTime = startTime + (frameIndex * 100 * NS_PER_MS);
+    float cloudScale = 1.0 + (float(frameIndex) / 50.0);
+
     google::protobuf::Timestamp* timestamp = pcl.mutable_timestamp();
-    timestamp->set_seconds(cloud_time / NS_PER_S);
-    timestamp->set_nanos(cloud_time % NS_PER_S);
-    std::string* data = pcl.mutable_data();
-    for (int point_index = 0; point_index < POINTS_PER_CLOUD; ++point_index) {
-      auto point = point_gen.next(1.0 + (float(frame_index) / 50.0));
-      auto base_offset = (point_index * sizeof(float) * FIELDS_PER_POINT);
-      write_float_little_endian(point.x, data, base_offset);
-      write_float_little_endian(point.y, data, base_offset + sizeof(float));
-      write_float_little_endian(point.z, data, base_offset + (2 * sizeof(float)));
-    }
+    timestamp->set_seconds(cloudTime / NS_PER_S);
+    timestamp->set_nanos(cloudTime % NS_PER_S);
 
+    std::string* data = pcl.mutable_data();
+    for (int pointIndex = 0; pointIndex < POINTS_PER_CLOUD; ++pointIndex) {
+      auto point = pointGenerator.next(cloudScale);
+      auto baseOffset = (pointIndex * sizeof(float) * FIELDS_PER_POINT);
+      WriteLittleEndianFloat(point.x, data, baseOffset);
+      WriteLittleEndianFloat(point.y, data, baseOffset + sizeof(float));
+      WriteLittleEndianFloat(point.z, data, baseOffset + (2 * sizeof(float)));
+    }
     std::string serialized;
     pcl.SerializeToString(&serialized);
 
     mcap::Message msg;
     msg.channelId = channel.id;
-    msg.sequence = frame_index;
-    msg.publishTime = cloud_time;
-    msg.logTime = cloud_time;
+    msg.sequence = frameIndex;
+    msg.publishTime = cloudTime;
+    msg.logTime = cloudTime;
     msg.data = (std::byte*)(serialized.data());
     msg.dataSize = serialized.size();
     const auto res = writer.write(msg);
@@ -136,10 +143,11 @@ int main(int, char**) {
       std::cerr << "Failed to write message: " << res.message << "\n";
       writer.terminate();
       out.close();
-      std::remove(OutputFilename);
+      std::remove(outputFilename);
       return 1;
     }
   }
   writer.close();
+  std::cerr << "wrote 100 pointcloud messages to " << outputFilename << std::endl;
   return 0;
 }
