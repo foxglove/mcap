adb4fde810097c8bb473419814159d38ce544cca
James Smith <james@foxglove.dev>
2022-07-14 16:05:03 +1000

    cpp: add JSON Schema examples

---
+++ cpp/examples/jsonschema/reader.cpp
@@ -0,0 +1,58 @@
+// Example code for reading JSON messages from an MCAP file.
+#define MCAP_IMPLEMENTATION
+#include "mcap/reader.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <string_view>
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <input.mcap>" << std::endl;
+    return 1;
+  }
+  const char* inputFilename = argv[1];
+
+  mcap::McapReader reader;
+  {
+    const auto res = reader.open(inputFilename);
+    if (!res.ok()) {
+      std::cerr << "Failed to open " << inputFilename << " for reading: " << res.message
+                << std::endl;
+      return 1;
+    }
+  }
+
+  auto messageView = reader.readMessages();
+
+  std::cout << "topic\ttype\ttimestamp\tfields" << std::endl;
+
+  for (auto it = messageView.begin(); it != messageView.end(); it++) {
+    // skip any non-json-encoded messages.
+    if (it->channel->messageEncoding != "json") {
+      continue;
+    }
+    std::string_view asString(reinterpret_cast<const char*>(it->message.data),
+                              it->message.dataSize);
+
+    auto parsed = nlohmann::json::parse(asString, nullptr, false);
+
+    if (parsed.is_discarded()) {
+      std::cerr << "failed to parse JSON: " << asString << std::endl;
+      reader.close();
+      return 1;
+    }
+    if (!parsed.is_object()) {
+      std::cerr << "unexpected non-object message: " << asString << std::endl;
+    }
+    std::cout << it->channel->topic << "\t(" << it->schema->name << ")\t[" << it->message.logTime
+              << "]:\t{ ";
+
+    for (auto it : parsed.items()) {
+      std::cout << it.key() << " ";
+    }
+    std::cout << "}" << std::endl;
+  }
+  reader.close();
+  return 0;
+}
---
+++ cpp/examples/jsonschema/writer.cpp
@@ -0,0 +1,98 @@
+// Example of writing JSON messages to an MCAP file in C++.
+// Writes a topic of Point2 messages.
+#define MCAP_IMPLEMENTATION
+#include "mcap/writer.hpp"
+
+#include <nlohmann/json.hpp>
+
+#include <chrono>
+#include <cmath>
+#include <iostream>
+
+#define NUM_FRAMES 100
+#define NS_PER_MS 1000000
+
+static const char* SCHEMA_NAME = "foxglove.Point2";
+static const char* SCHEMA_TEXT =
+  "{ \
+  \"$comment\": \"Generated from Point2 by @foxglove/schemas\", \
+  \"title\": \"Point2\", \
+  \"description\": \"A point representing a position in 2D space\", \
+  \"type\": \"object\", \
+  \"properties\": { \
+    \"x\": { \
+      \"type\": \"number\", \
+      \"description\": \"x coordinate position\" \
+    }, \
+    \"y\": { \
+      \"type\": \"number\", \
+      \"description\": \"y coordinate position\" \
+    } \
+  } \
+}";
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    std::cerr << "Usage: " << argv[0] << " <output.mcap>" << std::endl;
+    return 1;
+  }
+  const char* outputFilename = argv[1];
+
+  mcap::McapWriter writer;
+  {
+    auto options = mcap::McapWriterOptions("x-jsonschema");
+    const auto res = writer.open(outputFilename, options);
+    if (!res.ok()) {
+      std::cerr << "Failed to open " << outputFilename << " for writing: " << res.message
+                << std::endl;
+      return 1;
+    }
+  }
+  // Create a channel and schema for our messages.
+  // A message's channel informs the reader on the topic those messages were published on.
+  // A channel's schema informs the reader on how to interpret the messages' content.
+  // MCAP follows a relational model, where:
+  // * messages have a many-to-one relationship with channels (indicated by their channel_id)
+  // * channels have a many-to-one relationship with schemas (indicated by their schema_id)
+  mcap::ChannelId channelId;
+  {
+    mcap::Schema schema(SCHEMA_NAME, "jsonschema", SCHEMA_TEXT);
+    writer.addSchema(schema);
+
+    // choose an arbitrary topic name.
+    mcap::Channel channel("point", "json", schema.id);
+    writer.addChannel(channel);
+    channelId = channel.id;
+  }
+  mcap::Timestamp startTime = std::chrono::duration_cast<std::chrono::nanoseconds>(
+                                std::chrono::system_clock::now().time_since_epoch())
+                                .count();
+
+  for (uint64_t frame_index = 0; frame_index < NUM_FRAMES; ++frame_index) {
+    mcap::Timestamp frameTime = startTime + (frame_index * 100 * NS_PER_MS);
+
+    auto contentJson = nlohmann::json::object();
+    float p = (2.0 * 2.0 * M_PI * float(frame_index)) / float(NUM_FRAMES);
+    contentJson["x"] = sin(p);
+    contentJson["y"] = cos(p);
+    std::string serialized = contentJson.dump();
+
+    mcap::Message msg;
+    msg.channelId = channelId;
+    msg.logTime = frameTime;
+    msg.publishTime = frameTime;
+    msg.sequence = frame_index;
+    msg.data = reinterpret_cast<const std::byte*>(serialized.data());
+    msg.dataSize = serialized.size();
+    auto res = writer.write(msg);
+    if (!res.ok()) {
+      std::cerr << "failed to write message: " << res.message << std::endl;
+      writer.close();
+      return 1;
+    }
+  }
+  std::cout << "wrote " << NUM_FRAMES << " " << SCHEMA_NAME << " messages to " << outputFilename
+            << std::endl;
+  writer.close();
+  return 0;
+}
