6e7d9ad6705d4199448cd97f85bbd81cfa9bf21a
james-rms <james@foxglove.dev>
2022-07-21 08:17:59 +1000

    python: add ros2 example (#476)
    **Public-Facing Changes**
    Adds an example for users wanting to read or write MCAPs with ROS2.
    
    
    **Description**
    <!-- describe what has changed, and motivation behind those changes -->
    addresses #456.

---
+++ python/examples/ros2/entry.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+source /opt/ros/humble/setup.bash
+source install/local_setup.bash
+exec "$@"
---
+++ python/examples/ros2/py_mcap_demo/test/test_reader_writer.py
@@ -0,0 +1,34 @@
+from mcap.mcap0.reader import make_reader
+
+from py_mcap_demo.reader import read_messages
+from py_mcap_demo.writer import write_to
+
+from std_msgs.msg import String
+
+
+def test_write_then_read(tmp_path):
+    mcap_path = tmp_path / "ros2_bags"
+    write_to(str(mcap_path))
+
+    (filepath,) = tuple(mcap_path.glob("*.mcap"))
+
+    mcap_reader_count = 0
+    with open(filepath, "rb") as f:
+        reader = make_reader(f)
+        for schema, channel, _ in reader.iter_messages():
+            assert schema.encoding == "ros2msg"
+            assert schema.name == "std_msgs/msg/String"
+            assert channel.message_encoding == "cdr"
+            assert channel.topic == "/chatter"
+            mcap_reader_count += 1
+
+    assert mcap_reader_count == 10
+
+    rosbag_reader_count = 0
+    for topic, msg, timestamp in read_messages(str(mcap_path)):
+        assert topic == "/chatter"
+        assert type(msg) == String
+        assert msg.data.startswith("Chatter")
+        rosbag_reader_count += 1
+
+    assert rosbag_reader_count == 10
