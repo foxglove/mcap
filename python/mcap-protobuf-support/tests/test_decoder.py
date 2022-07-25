593f145db61da67daad32faca1a78f3472ff95dd
james-rms <james@foxglove.dev>
2022-07-26 08:05:37 +1000

    mcap_protobuf: add writer.py (#487)
    **Public-Facing Changes**
    
    Adds a higher-level MCAP writer abstraction for the `mcap-protobuf-support` library. This was the user doesn't need to think about schema IDs or channel IDs, they just write messages.

--- python/mcap-protobuf-support/tests/test_decoder.py
+++ python/mcap-protobuf-support/tests/test_decoder.py
@@ -1,4 +1,3 @@
-import hashlib
 from io import BytesIO, RawIOBase
 from typing import cast
 
@@ -11,11 +10,7 @@ from .generate import generate_sample_data
 def test_protobuf_decoder():
     output = BytesIO()
     generate_sample_data(output)
-    assert (
-        hashlib.sha256(output.getvalue()).hexdigest()
-        == "6d656a7fe9f2e591c97d51f872519bd159a9dcd1b4b1610809b4d8efb3b6ec5a"
-    )
     reader = StreamReader(cast(RawIOBase, output))
     decoder = Decoder(reader)
-    messages = [m for m in decoder.messages]
+    messages = list(decoder.messages)
     assert len(messages) == 20
