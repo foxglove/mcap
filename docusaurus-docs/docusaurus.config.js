55a2b3a4266ccef8f17909a4999d203da2ce82db
James Smith <james@foxglove.dev>
2022-08-04 16:16:23 +1000

    build in ci

--- docusaurus-docs/docusaurus.config.js
+++ docusaurus-docs/docusaurus.config.js
@@ -16,8 +16,10 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'foxglove', // Usually your GitHub org/user name.
+  projectName: 'mcap', // Usually your repo name.
+  deploymentBranch: 'gh-pages',
+  trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
