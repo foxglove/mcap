be6d8721f2b5eba529ae8ea0dc8542d531b0a8a7
James Smith <james@foxglove.dev>
2022-08-05 10:42:22 +1000

    Trailing slash is true

--- docusaurus-docs/docusaurus.config.js
+++ docusaurus-docs/docusaurus.config.js
@@ -19,7 +19,7 @@ const config = {
   organizationName: 'foxglove', // Usually your GitHub org/user name.
   projectName: 'mcap', // Usually your repo name.
   deploymentBranch: 'gh-pages',
-  trailingSlash: false,
+  trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
