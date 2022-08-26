8e8d98c8def0298a5a32fb756dac010f3f6c9b3c
James Smith <james@foxglove.dev>
2022-08-26 14:03:50 +1000

    test file ordering

--- go/mcap/range_index_heap_test.go
+++ go/mcap/range_index_heap_test.go
@@ -1,42 +1,88 @@
 package mcap
 
 import (
-	"container/heap"
+	"reflect"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestForwardOrdering(t *testing.T) {
-	h := &rangeIndexHeap{order: ReadOrderLogTime}
-	heap.Push(h, rangeIndex{chunkIndex: &ChunkIndex{MessageStartTime: 0}})
-	heap.Push(h, rangeIndex{messageIndexEntry: &MessageIndexEntry{Timestamp: 1}})
-	heap.Push(h, rangeIndex{chunkIndex: &ChunkIndex{MessageStartTime: 3}})
-	heap.Push(h, rangeIndex{messageIndexEntry: &MessageIndexEntry{Timestamp: 2}})
-
-	assert.Equal(t, h.Len(), 4)
-
-	assert.Equal(t, rangeIndex{chunkIndex: &ChunkIndex{MessageStartTime: 0}}, heap.Pop(h))
-	assert.Equal(t, rangeIndex{messageIndexEntry: &MessageIndexEntry{Timestamp: 1}}, heap.Pop(h))
-	assert.Equal(t, rangeIndex{messageIndexEntry: &MessageIndexEntry{Timestamp: 2}}, heap.Pop(h))
-	assert.Equal(t, rangeIndex{chunkIndex: &ChunkIndex{MessageStartTime: 3}}, heap.Pop(h))
-
-	assert.Equal(t, h.Len(), 0)
+var rangeIndexHeapTestItems = []rangeIndex{
+	{
+		chunkIndex: &ChunkIndex{
+			ChunkStartOffset: 1,
+			MessageStartTime: 100,
+			MessageEndTime:   300,
+		},
+	},
+	{
+		chunkIndex: &ChunkIndex{
+			ChunkStartOffset: 2,
+			MessageStartTime: 200,
+			MessageEndTime:   400,
+		},
+		messageIndexEntry: &MessageIndexEntry{Offset: 3, Timestamp: 200},
+	},
+	{
+		chunkIndex: &ChunkIndex{
+			ChunkStartOffset: 2,
+			MessageStartTime: 200,
+			MessageEndTime:   400,
+		},
+		messageIndexEntry: &MessageIndexEntry{Offset: 2, Timestamp: 250},
+	},
+	{
+		chunkIndex: &ChunkIndex{
+			ChunkStartOffset: 3,
+			MessageStartTime: 300,
+			MessageEndTime:   400,
+		},
+	},
 }
 
-func TestReverseOrdering(t *testing.T) {
-	h := &rangeIndexHeap{order: ReadOrderReverseLogTime}
-	heap.Push(h, rangeIndex{chunkIndex: &ChunkIndex{MessageEndTime: 0}})
-	heap.Push(h, rangeIndex{messageIndexEntry: &MessageIndexEntry{Timestamp: 1}})
-	heap.Push(h, rangeIndex{chunkIndex: &ChunkIndex{MessageEndTime: 3}})
-	heap.Push(h, rangeIndex{messageIndexEntry: &MessageIndexEntry{Timestamp: 2}})
-
-	assert.Equal(t, h.Len(), 4)
-
-	assert.Equal(t, rangeIndex{chunkIndex: &ChunkIndex{MessageEndTime: 3}}, heap.Pop(h))
-	assert.Equal(t, rangeIndex{messageIndexEntry: &MessageIndexEntry{Timestamp: 2}}, heap.Pop(h))
-	assert.Equal(t, rangeIndex{messageIndexEntry: &MessageIndexEntry{Timestamp: 1}}, heap.Pop(h))
-	assert.Equal(t, rangeIndex{chunkIndex: &ChunkIndex{MessageEndTime: 0}}, heap.Pop(h))
-
-	assert.Equal(t, h.Len(), 0)
+func TestMessageOrdering(t *testing.T) {
+	cases := []struct {
+		assertion          string
+		order              ReadOrder
+		expectedIndexOrder []int
+	}{
+		{
+			assertion:          "read time order forwards",
+			order:              ReadOrderLogTime,
+			expectedIndexOrder: []int{0, 1, 2, 3},
+		},
+		{
+			assertion:          "read time order backwards",
+			order:              ReadOrderReverseLogTime,
+			expectedIndexOrder: []int{3, 0, 2, 1},
+		},
+		{
+			assertion:          "read file order",
+			order:              ReadOrderFile,
+			expectedIndexOrder: []int{0, 2, 1, 3},
+		},
+	}
+	for _, c := range cases {
+		t.Run(c.assertion, func(t *testing.T) {
+			h := &rangeIndexHeap{order: c.order}
+			for _, item := range rangeIndexHeapTestItems {
+				assert.Nil(t, h.HeapPush(item))
+			}
+			assert.Equal(t, h.Len(), len(rangeIndexHeapTestItems))
+			i := 0
+			for h.Len() > 0 {
+				popped_item, err := h.HeapPop()
+				assert.Nil(t, err)
+				found := false
+				for index, item := range rangeIndexHeapTestItems {
+					if reflect.DeepEqual(item, *popped_item) {
+						assert.Equal(t, c.expectedIndexOrder[i], index)
+						found = true
+					}
+				}
+				assert.True(t, found)
+				i++
+			}
+		})
+	}
 }
