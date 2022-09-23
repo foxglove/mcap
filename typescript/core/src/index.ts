1cd427674354795cb28fcf5f106716fcc202bfdf
John Hurliman <jhurliman@jhurliman.org>
2022-09-23 10:37:58 -0700

    Remove pre0 support from typescript library (#602)

--- typescript/core/src/index.ts
+++ typescript/core/src/index.ts
@@ -1,7 +1,3 @@
-export { default as McapPre0Reader } from "./pre0/McapPre0Reader";
-export { default as McapPre0Writer } from "./pre0/McapPre0Writer";
-export * as McapPre0Types from "./pre0/types";
-
 export { Mcap0IndexedReader } from "./v0/Mcap0IndexedReader";
 export { default as Mcap0StreamReader } from "./v0/Mcap0StreamReader";
 export { Mcap0Writer } from "./v0/Mcap0Writer";
@@ -12,4 +8,4 @@ export * as Mcap0Types from "./v0/types";
 export * as Mcap0Constants from "./v0/constants";
 export type { IWritable } from "./common/IWritable";
 
-export * from "./common/detectVersion";
+export * from "./common/hasMcapPrefix";
