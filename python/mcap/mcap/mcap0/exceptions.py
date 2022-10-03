76e6f56a4fc92c4c95bc2d30e4f53426e7fb5700
James Smith <james@foxglove.dev>
2022-10-04 07:59:54 +1100

    move mcap0 modules to mcap

--- python/mcap/mcap/mcap0/exceptions.py
+++ python/mcap/mcap/mcap0/exceptions.py
@@ -1,11 +1,1 @@
-class McapError(Exception):
-    pass
-
-
-class InvalidMagic(McapError):
-    def __init__(self):
-        super().__init__("not a valid MCAP file")
-
-
-class EndOfFile(McapError):
-    pass
+from ..exceptions import *
