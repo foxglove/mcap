6ca60465a35cd7f690cabbb58a426a3cc122d9cd
snosenzo <sam@foxglove.dev>
2022-10-27 22:04:56 +0200

    - making grid mcap

--- typescript/examples/basicwriter/scripts/main.ts
+++ typescript/examples/basicwriter/scripts/main.ts
@@ -1,5 +1,14 @@
+import { Grid, FrameTransform, NumericType } from "@foxglove/schemas";
+import {
+  Grid as GridSchema,
+  FrameTransform as FrameTransformSchema,
+} from "@foxglove/schemas/jsonschema";
 import { McapWriter, IWritable } from "@mcap/core";
 import { open, FileHandle } from "fs/promises";
+
+import { Message } from "../../../core/src/types";
+
+const QUAT_IDENTITY = { x: 0, y: 0, z: 0, w: 1 };
 
 // Mcap IWritable interface for nodejs FileHandle
 class FileHandleWritable implements IWritable {
@@ -19,6 +28,124 @@ class FileHandleWritable implements IWritable {
     return BigInt(this.totalBytesWritten);
   }
 }
+function roundUp(numToRound: number, multiple: number) {
+  if (multiple !== 0 && (multiple & (multiple - 1)) === 0) {
+    return (numToRound + multiple - 1) & -multiple;
+  } else {
+    throw Error(`invalid multiple ${multiple}, num to round ${numToRound}`);
+  }
+}
+const scriptParameters = {
+  mcapTimeLength: 5000, //ms
+  gridMessageFrequency: 10, //hz
+};
+
+const gridParameters = {
+  cell_size: { x: 10, y: 10 },
+  column_count: 1024,
+  row_count: 1024,
+  cell_stride: 4,
+  row_stride: 0,
+};
+gridParameters.row_stride = roundUp(gridParameters.column_count * gridParameters.cell_stride, 2);
+console.log(`row stride ${gridParameters.row_stride}`);
+
+interface GridDataFuncParams {
+  x: number;
+  y: number;
+  i: number;
+  rows: number;
+  cols: number;
+  z: number;
+}
+
+const fieldDataFuncs = {
+  sinpluscos: ({ x, y }: GridDataFuncParams): number => {
+    return Math.sin(y) + Math.cos(x);
+  },
+  sinmultcos: ({ x, y }: GridDataFuncParams): number => {
+    return Math.sin(y) * Math.cos(x);
+  },
+  sintancos: ({ x, y }: GridDataFuncParams): number => {
+    return Math.sin(Math.cos(Math.tan(x))) + Math.sin(Math.cos(Math.tan(y)));
+  },
+} as { [k: string]: (x: GridDataFuncParams) => number };
+
+function makeNewGridFromParamsWithoutData() {
+  const { cell_size, cell_stride, column_count, row_count, row_stride } = gridParameters;
+
+  const defaultGrid = {
+    timestamp: { sec: 0, nsec: 0 },
+    frame_id: "sensor",
+    pose: {
+      position: {
+        x: -0.5 * cell_size.x * row_count + 2,
+        y: -0.5 * cell_size.y * column_count + 2,
+        z: 0,
+      },
+      orientation: QUAT_IDENTITY,
+    },
+    cell_size,
+    column_count,
+    cell_stride,
+    row_stride,
+    fields: [{ name: "sintancos", offset: 0, type: NumericType.FLOAT32 }],
+  } as Omit<Grid, "data">;
+
+  return defaultGrid;
+}
+
+function makeDataForGridFields(grid: Omit<Grid, "data">, row_count: number, zFactor: number) {
+  const { column_count, fields, cell_stride, row_stride } = grid;
+  const data = new Uint8Array(row_stride * row_count);
+  const view = new DataView(data.buffer, data.byteOffset, data.byteLength);
+
+  for (let y = 0; y < row_count; y++) {
+    for (let x = 0; x < column_count; x++) {
+      const i = y * row_stride + x * cell_stride;
+      for (const field of fields) {
+        const { name, offset, type } = field;
+        if (type !== NumericType.FLOAT32) {
+          throw new Error("unsupported numeric types");
+        }
+        const dataFunc = fieldDataFuncs[name];
+        if (dataFunc) {
+          const value = dataFunc({
+            x,
+            y,
+            i,
+            rows: row_count,
+            cols: column_count,
+            z: zFactor,
+          });
+          view.setFloat32(i + offset, value);
+        } else {
+          throw new Error("missing data func " + name);
+        }
+      }
+    }
+  }
+
+  (grid as Grid).data = data;
+}
+
+async function addMessageAtTime(
+  mcapFile: McapWriter,
+  gridChannelId: number,
+  getMessageData: () => Message["data"],
+  time: bigint,
+): Promise<void> {
+  console.log('getting message');
+  const msgData = Buffer.from(JSON.stringify(getMessageData()));
+  console.log(`writing message at ${time}`);
+  await mcapFile.addMessage({
+    channelId: gridChannelId,
+    sequence: 0,
+    publishTime: time,
+    logTime: time,
+    data: msgData,
+  });
+}
 
 async function main() {
   const mcapFilePath = "output.mcap";
@@ -36,43 +163,68 @@ async function main() {
     profile: "",
     library: "mcap example",
   });
-
-  const schema = {
-    title: "HelloWorld",
-    type: "object",
-    properties: {
-      value: {
-        type: "string",
-      },
-    },
-  };
-
-  const schemaId = await mcapFile.registerSchema({
-    name: schema.title,
+  const tfSchemaId = await mcapFile.registerSchema({
+    name: "foxglove.FrameTransform",
     encoding: "jsonschema",
-    data: Buffer.from(JSON.stringify(schema)),
+    data: Buffer.from(JSON.stringify(FrameTransformSchema)),
   });
 
-  const channelId = await mcapFile.registerChannel({
-    schemaId,
-    topic: "some_topic",
+  const tfChannelId = await mcapFile.registerChannel({
+    schemaId: tfSchemaId,
+    topic: "tf",
     messageEncoding: "json",
     metadata: new Map(),
   });
 
-  const msgData = Buffer.from(
-    JSON.stringify({
-      value: "hello world!",
-    }),
-  );
-
   await mcapFile.addMessage({
-    channelId,
+    channelId: tfChannelId,
     sequence: 0,
     publishTime: 0n,
-    logTime: BigInt(Date.now()) * 1_000_000n,
-    data: msgData,
+    logTime: 0n,
+    data: Buffer.from(
+      JSON.stringify({
+        timestamp: { sec: 0, nsec: 0 },
+        parent_frame_id: "base_link",
+        child_frame_id: "sensor",
+        translation: { x: 0, y: 0, z: 1 },
+        rotation: QUAT_IDENTITY,
+      } as FrameTransform),
+    ),
   });
+
+  const gridSchemaId = await mcapFile.registerSchema({
+    name: "foxglove.Grid",
+    encoding: "jsonschema",
+    data: Buffer.from(JSON.stringify(GridSchema)),
+  });
+
+  const gridChannelId = await mcapFile.registerChannel({
+    schemaId: gridSchemaId,
+    topic: "grid",
+    messageEncoding: "json",
+    metadata: new Map(),
+  });
+
+  const { mcapTimeLength, gridMessageFrequency } = scriptParameters;
+
+  const timeBetweenMessages = 1000 / gridMessageFrequency; // 1s (ms) / frequency
+  let currTime = 0;
+  let z = 0;
+  const getGridMessageData = () => {
+    const grid = makeNewGridFromParamsWithoutData();
+    makeDataForGridFields(grid, gridParameters.row_count, z);
+    z++;
+    return Buffer.from(JSON.stringify(grid));
+  };
+  let count = 0;
+  const proms = [];
+  while (currTime < mcapTimeLength) {
+    console.log(`Adding grid ${count}`);
+    proms.push(addMessageAtTime(mcapFile, gridChannelId, getGridMessageData, BigInt(currTime)));
+    count++;
+    currTime += timeBetweenMessages;
+  }
+  await Promise.all(proms);
 
   await mcapFile.end();
 }
