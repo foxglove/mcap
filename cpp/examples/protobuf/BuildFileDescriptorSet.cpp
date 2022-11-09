b6680aebd7e3d85482b3e5a53bf0000e83ea5dd4
Jacob Bandes-Storch <jacob@foxglove.dev>
2022-11-09 15:40:01 -0800

    Fix FileDescriptorSet generation & add test (#717)
    **Public-Facing Changes**
    Fixed FileDescriptorSet generation in protobuf examples to avoid adding duplicate types via transitive dependencies.
    
    **Description**
    Supersedes / closes https://github.com/foxglove/mcap/pull/694
    
    Copied the new implementation from https://github.com/foxglove/ws-protocol/pull/269

---
+++ cpp/examples/protobuf/BuildFileDescriptorSet.cpp
@@ -0,0 +1,31 @@
+#include "BuildFileDescriptorSet.h"
+
+#include <queue>
+#include <unordered_set>
+
+namespace foxglove {
+
+/// Builds a FileDescriptorSet of this descriptor and all transitive dependencies, for use as a
+/// channel schema.
+google::protobuf::FileDescriptorSet BuildFileDescriptorSet(
+  const google::protobuf::Descriptor* toplevelDescriptor) {
+  google::protobuf::FileDescriptorSet fdSet;
+  std::queue<const google::protobuf::FileDescriptor*> toAdd;
+  toAdd.push(toplevelDescriptor->file());
+  std::unordered_set<std::string> seenDependencies;
+  while (!toAdd.empty()) {
+    const google::protobuf::FileDescriptor* next = toAdd.front();
+    toAdd.pop();
+    next->CopyTo(fdSet.add_file());
+    for (int i = 0; i < next->dependency_count(); ++i) {
+      const auto& dep = next->dependency(i);
+      if (seenDependencies.find(dep->name()) == seenDependencies.end()) {
+        seenDependencies.insert(dep->name());
+        toAdd.push(dep);
+      }
+    }
+  }
+  return fdSet;
+}
+
+}  // namespace foxglove
