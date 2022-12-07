6780a2c0a3ce775a4227ace5d334eea9505fb94c
James Smith <james@foxglove.dev>
2022-12-08 10:21:52 +1100

    conformance: add test input with out-of-order messages

--- tests/conformance/variants/inputs.ts
+++ tests/conformance/variants/inputs.ts
@@ -1,4 +1,17 @@
-import { TestInput } from "./types";
+import { TestInput, TestDataRecord } from "./types";
+
+const tenMessages: TestDataRecord[] = [0n, 2n, 1n, 3n, 3n, 5n, 4n, 7n, 8n, 9n].map(
+  (logTime, sequence) => {
+    return {
+      type: "Message",
+      channelId: 1,
+      publishTime: logTime,
+      logTime,
+      data: new Uint8Array([1, 2, 3]),
+      sequence,
+    };
+  },
+);
 
 const inputs: TestInput[] = [
   { baseName: "NoData", records: [] },
@@ -46,6 +59,27 @@ const inputs: TestInput[] = [
   {
     baseName: "OneMetadata",
     records: [{ type: "Metadata", name: "myMetadata", metadata: new Map([["foo", "bar"]]) }],
+  },
+  {
+    baseName: "TenMessages",
+    records: [
+      {
+        type: "Schema",
+        id: 1,
+        name: "Example",
+        encoding: "c",
+        data: new Uint8Array([4, 5, 6]),
+      },
+      {
+        type: "Channel",
+        id: 1,
+        topic: "example",
+        schemaId: 1,
+        messageEncoding: "a",
+        metadata: new Map([["foo", "bar"]]),
+      },
+      ...tenMessages,
+    ],
   },
 ];
 
