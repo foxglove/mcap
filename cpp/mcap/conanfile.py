4449a23b69f0ffc728f0288fe464caf86f959f9f
James Smith <james@foxglove.dev>
2023-01-06 13:36:14 +1100

    cpp: bump version number

--- cpp/mcap/conanfile.py
+++ cpp/mcap/conanfile.py
@@ -3,7 +3,7 @@ from conans import ConanFile, tools
 
 class McapConan(ConanFile):
     name = "mcap"
-    version = "0.8.0"
+    version = "0.8.1"
     url = "https://github.com/foxglove/mcap"
     homepage = "https://github.com/foxglove/mcap"
     description = "A C++ implementation of the MCAP file format"
