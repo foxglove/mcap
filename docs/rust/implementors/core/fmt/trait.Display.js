8635bcbe32e9a089746e9474ce3166c04bceb8c4
Foxglove <contact@foxglove.dev>
2023-01-12 01:04:48 +0000

    Update docs from main (1d055ce)

--- docs/rust/implementors/core/fmt/trait.Display.js
+++ docs/rust/implementors/core/fmt/trait.Display.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"mcap":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.0/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"mcap/enum.McapError.html\" title=\"enum mcap::McapError\">McapError</a>"]]
+"mcap":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"mcap/enum.McapError.html\" title=\"enum mcap::McapError\">McapError</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
