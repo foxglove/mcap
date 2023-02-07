1c86ab40945bdb73118a3b55e0ee8364608d5c32
James Smith <james@foxglove.dev>
2023-02-07 11:38:56 +1100

    cpp: add conan lockfiles for leaf packaegs

--- cpp/build.sh
+++ cpp/build.sh
@@ -6,13 +6,13 @@ conan config init
 
 conan editable add ./mcap mcap/0.9.0
 conan install test --install-folder test/build/Debug \
-  -s compiler.cppstd=17 -s build_type=Debug --build missing
+  -s compiler.cppstd=17 -s build_type=Debug --build missing -l test/conan.lock
 
 if [ "$1" != "--build-tests-only" ]; then
   conan install bench --install-folder bench/build/Release \
-    -s compiler.cppstd=17 -s build_type=Release --build missing
+    -s compiler.cppstd=17 -s build_type=Release --build missing -l bench/conan.lock
   conan install examples --install-folder examples/build/Release \
-    -s compiler.cppstd=17 -s build_type=Release --build missing
+    -s compiler.cppstd=17 -s build_type=Release --build missing -l examples/conan.lock
   conan build examples --build-folder examples/build/Release
   conan build bench --build-folder bench/build/Release
 fi
