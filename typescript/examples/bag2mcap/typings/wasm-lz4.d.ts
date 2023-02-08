627138103f063dee183ca3f9da7d09fedfa17a21
John Hurliman <jhurliman@jhurliman.org>
2023-02-08 16:43:14 -0700

    Use @foxglove/wasm-lz4 and @foxglove/wasm-zstd libraries (#808)

--- typescript/examples/bag2mcap/typings/wasm-lz4.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "wasm-lz4" {
-  function decompress(buffer: Uint8Array, size: number): Buffer;
-  namespace decompress {
-    const isLoaded: Promise<boolean>;
-  }
-
-  export default decompress;
-}
