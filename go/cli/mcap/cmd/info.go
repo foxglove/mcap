a6ffb3bc8268534fe218052e84a0c110fc534d3c
James Smith <james@foxglove.dev>
2023-03-06 11:51:35 +1100

    CLI: info: handle durations > 2**63

--- go/cli/mcap/cmd/info.go
+++ go/cli/mcap/cmd/info.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"fmt"
 	"io"
+	"math"
 	"os"
 	"sort"
 	"time"
@@ -19,9 +20,8 @@ var (
 )
 
 func decimalTime(t time.Time) string {
-	unix := t.UnixNano()
-	seconds := unix / 1e9
-	nanoseconds := unix % 1e9
+	seconds := t.Unix()
+	nanoseconds := t.Nanosecond()
 	return fmt.Sprintf("%d.%09d", seconds, nanoseconds)
 }
 
@@ -45,16 +45,23 @@ func printInfo(w io.Writer, info *mcap.Info) error {
 		fmt.Fprintf(buf, "messages: %d\n", info.Statistics.MessageCount)
 		start = info.Statistics.MessageStartTime
 		end = info.Statistics.MessageEndTime
+		durationNs := float64(end) - float64(start)
+		durationInSeconds = durationNs / 1e9
 		starttime := time.Unix(int64(start/1e9), int64(start%1e9))
 		endtime := time.Unix(int64(end/1e9), int64(end%1e9))
-		fmt.Fprintf(buf, "duration: %s\n", endtime.Sub(starttime))
-		durationInSeconds = endtime.Sub(starttime).Seconds()
+		if math.Abs(durationNs) > math.MaxInt64 {
+			// time.Duration is an int64 nanosecond count under the hood, but end and start can
+			// be further apart than that.
+			fmt.Fprintf(buf, "duration: %.3fs\n", durationInSeconds)
+		} else {
+			fmt.Fprintf(buf, "duration: %s\n", endtime.Sub(starttime))
+		}
 		if starttime.After(LongAgo) {
 			fmt.Fprintf(buf, "start: %s (%s)\n", starttime.Format(time.RFC3339Nano), decimalTime(starttime))
 			fmt.Fprintf(buf, "end: %s (%s)\n", endtime.Format(time.RFC3339Nano), decimalTime(endtime))
 		} else {
-			fmt.Fprintf(buf, "start: %.3f\n", float64(starttime.UnixNano())/1e9)
-			fmt.Fprintf(buf, "end: %.3f\n", float64(endtime.UnixNano())/1e9)
+			fmt.Fprintf(buf, "start: %s\n", decimalTime(starttime))
+			fmt.Fprintf(buf, "end: %s\n", decimalTime(endtime))
 		}
 	}
 	if len(info.ChunkIndexes) > 0 {
