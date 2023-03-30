9e0aab38230a28b6ed22862cda24281fe091c021
James Smith <james@foxglove.dev>
2023-03-30 11:59:15 +1100

    python-ros2-support: provide type and full text attributes

--- python/mcap-ros2-support/tests/test_ros2_decoder.py
+++ python/mcap-ros2-support/tests/test_ros2_decoder.py
@@ -10,7 +10,10 @@ def test_ros2_decoder():
         decoder = Decoder()
         count = 0
         for index, (schema, _, message) in enumerate(reader.iter_messages()):
+            assert schema is not None
             ros_msg = decoder.decode(schema, message)
             assert ros_msg.data == f"string message {index}"
+            assert ros_msg._type == "std_msgs/String"
+            assert ros_msg._full_text == "# std_msgs/String\nstring data"
             count += 1
         assert count == 10
