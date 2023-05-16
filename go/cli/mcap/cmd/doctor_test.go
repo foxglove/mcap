ae949dc46176af7cd2cd5c808c118e7b8861dea0
James Smith <james@foxglove.dev>
2023-05-16 12:53:43 +1000

    doctor: add test for schemaless, messageless chunks

---
+++ go/cli/mcap/cmd/doctor_test.go
@@ -0,0 +1,31 @@
+package cmd
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/foxglove/mcap/go/mcap"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNoErrorOnMessagelessChunks(t *testing.T) {
+	buf := bytes.Buffer{}
+	writer, err := mcap.NewWriter(&buf, &mcap.WriterOptions{
+		Chunked:   true,
+		ChunkSize: 10,
+	})
+	assert.Nil(t, err)
+	assert.Nil(t, writer.WriteHeader(&mcap.Header{"", ""}))
+	assert.Nil(t, writer.WriteChannel(&mcap.Channel{
+		ID:       1,
+		SchemaID: 0,
+		Topic:    "schemaless_topic",
+	}))
+	assert.Nil(t, writer.Close())
+
+	rs := bytes.NewReader(buf.Bytes())
+
+	doctor := newMcapDoctor(rs)
+	err = doctor.Examine()
+	assert.Nil(t, err)
+}
