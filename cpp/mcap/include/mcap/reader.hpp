27c085b96cc365884489910869ffcaad999144cc
Emerson Knapp <537409+emersonknapp@users.noreply.github.com>
2023-05-30 13:36:06 -0700

    CPP: add getter for metadata indices (#898)
    Signed-off-by: Emerson Knapp <emerson.b.knapp@gmail.com>

--- cpp/mcap/include/mcap/reader.hpp
+++ cpp/mcap/include/mcap/reader.hpp
@@ -428,6 +428,13 @@ public:
    */
   const std::vector<ChunkIndex>& chunkIndexes() const;
 
+  /**
+   * @brief Returns all of the parsed MetadataIndex records. Call `readSummary()`
+   * first to fully populate this data structure.
+   * The multimap's keys are the `name` field from each indexed Metadata.
+   */
+  const std::multimap<std::string, MetadataIndex>& metadataIndexes() const;
+
   // The following static methods are used internally for parsing MCAP records
   // and do not need to be called directly unless you are implementing your own
   // reader functionality or tests.
