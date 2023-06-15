ea82f3c077e8ae474853a199a79c403748f330a1
James Smith <james@foxglove.dev>
2023-06-16 09:58:05 +1000

    update examples

--- python/examples/ros2-noenv/reader.py
+++ python/examples/ros2-noenv/reader.py
@@ -1,14 +1,14 @@
 import sys
 
 from mcap.reader import make_reader
-from mcap_ros1.decoder import DecoderFactory
+from mcap_ros2.decoder import DecoderFactory
 
 
 def main():
     with open(sys.argv[1], "rb") as infile:
         reader = make_reader(infile, decoder_factories=[DecoderFactory()])
         for schema, channel, message, ros_msg in reader.iter_decoded_messages():
-            print(f"{channel.topic} {schema.name} [{message.log_time}]: {ros_msg.data}")
+            print(f"{channel.topic} {schema.name} [{message.log_time}]: {ros_msg}")
 
 
 if __name__ == "__main__":
