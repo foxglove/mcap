3b205c5b68d421278a6bf9608fc62d3278ab0086
James Smith <james@foxglove.dev>
2023-06-16 10:05:37 +1000

    fix lint

--- python/mcap/mcap/decoder.py
+++ python/mcap/mcap/decoder.py
@@ -5,10 +5,11 @@ from .records import Schema
 
 
 class DecoderFactory(ABC):
-    """ Provides functionality to an :py:class:`mcap.reader.McapReader` to decode message contents.
+    """Provides functionality to an :py:class:`mcap.reader.McapReader` to decode message contents.
     McapReader classes can be instantiated with a set of DecoderFactories,
     which are used within :py:func:`mcap.reader.McapReader.iter_decoded_messages`.
     """
+
     def decoder_for(
         self, message_encoding: str, schema: Optional[Schema]
     ) -> Optional[Callable[[bytes], Any]]:
