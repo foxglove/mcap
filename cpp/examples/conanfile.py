1c191e0aee2b1b9e59be6bb4ff9175c49d25be5e
james-rms <james@foxglove.dev>
2023-09-12 10:18:38 +1000

    fix: LinearMessageView begin does not compare equal to begin (#969)
    ### Public-Facing Changes
    
    bugfix: Multiple iterators created from LinearMessageView::begin() will compare to `true`.
    ### Description
    
    <!-- describe what has changed, and motivation behind those changes -->
    
    <!-- Link relevant Github issues. Use `Fixes #1234` to auto-close the
    issue after merging. -->

--- cpp/examples/conanfile.py
+++ cpp/examples/conanfile.py
@@ -5,7 +5,7 @@ class McapExamplesConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
     requires = [
-        "mcap/1.2.0",
+        "mcap/1.2.1",
         "protobuf/3.21.1",
         "nlohmann_json/3.10.5",
         "catch2/2.13.8",
