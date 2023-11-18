8fdfa6634bb772b72c24c2af5cc42dbd97380696
Jacob Bandes-Storch <jacob@foxglove.dev>
2023-11-17 18:41:17 -0800

    lint & yarn fixes

--- typescript/nodejs/src/FileHandleReadable.ts
+++ typescript/nodejs/src/FileHandleReadable.ts
@@ -5,26 +5,26 @@ import { FileHandle } from "fs/promises";
  * IReadable implementation for FileHandle.
  */
 export class FileHandleReadable implements McapTypes.IReadable {
-  private handle: FileHandle;
-  private buffer = new ArrayBuffer(4096);
+  #handle: FileHandle;
+  #buffer = new ArrayBuffer(4096);
 
   constructor(handle: FileHandle) {
-    this.handle = handle;
+    this.#handle = handle;
   }
 
   async size(): Promise<bigint> {
-    return BigInt((await this.handle.stat()).size);
+    return BigInt((await this.#handle.stat()).size);
   }
 
   async read(offset: bigint, length: bigint): Promise<Uint8Array> {
     if (offset > Number.MAX_SAFE_INTEGER || length > Number.MAX_SAFE_INTEGER) {
       throw new Error(`Read too large: offset ${offset}, length ${length}`);
     }
-    if (length > this.buffer.byteLength) {
-      this.buffer = new ArrayBuffer(Number(length * 2n));
+    if (length > this.#buffer.byteLength) {
+      this.#buffer = new ArrayBuffer(Number(length * 2n));
     }
-    const result = await this.handle.read({
-      buffer: new DataView(this.buffer, 0, Number(length)),
+    const result = await this.#handle.read({
+      buffer: new DataView(this.#buffer, 0, Number(length)),
       position: Number(offset),
     });
     if (result.bytesRead !== Number(length)) {
--- typescript/nodejs/src/FileHandleWritable.ts
+++ typescript/nodejs/src/FileHandleWritable.ts
@@ -5,19 +5,19 @@ import { FileHandle } from "fs/promises";
  * IWritable implementation for FileHandle.
  */
 export class FileHandleWritable implements IWritable {
-  private handle: FileHandle;
-  private totalBytesWritten = 0;
+  #handle: FileHandle;
+  #totalBytesWritten = 0;
 
   constructor(handle: FileHandle) {
-    this.handle = handle;
+    this.#handle = handle;
   }
 
   async write(buffer: Uint8Array): Promise<void> {
-    const written = await this.handle.write(buffer);
-    this.totalBytesWritten += written.bytesWritten;
+    const written = await this.#handle.write(buffer);
+    this.#totalBytesWritten += written.bytesWritten;
   }
 
   position(): bigint {
-    return BigInt(this.totalBytesWritten);
+    return BigInt(this.#totalBytesWritten);
   }
 }
