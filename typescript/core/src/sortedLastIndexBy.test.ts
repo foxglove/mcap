085d540fa2b2534ecd9b665e94a4ee34d407dbc0
Hans-Joachim Krauch <achim-k@users.noreply.github.com>
2024-01-12 12:23:41 -0300

    @mcap/core: Replace message index heap with sorted list (#1037)
    ### Public-Facing Changes
    
    @mcap/core: Replace message index heap with sorted list
    
    ### Description
    Instead of using a heap for storing message offsets, we read all message
    offsets into a sorted array (sorted by logTime) which allows for faster
    retrieval of next messages.
    
    ---------
    
    Co-authored-by: Roman Shtylman <roman@foxglove.dev>

---
+++ typescript/core/src/sortedLastIndexBy.test.ts
@@ -0,0 +1,69 @@
+import { sortedLastIndexBy } from "./sortedLastIndex";
+
+describe("sortedLastIndexBy", () => {
+  it("handles an empty array", () => {
+    const array: [bigint, bigint][] = [];
+
+    expect(sortedLastIndexBy(array, 0n, (x) => x)).toEqual(0);
+    expect(sortedLastIndexBy(array, 42n, (x) => x)).toEqual(0);
+  });
+
+  it("handles a contiguous array", () => {
+    const array: [bigint, bigint][] = [
+      [1n, 42n],
+      [2n, 42n],
+      [3n, 42n],
+    ];
+
+    expect(sortedLastIndexBy(array, 0n, (x) => x)).toEqual(0);
+    expect(sortedLastIndexBy(array, 1n, (x) => x)).toEqual(1);
+    expect(sortedLastIndexBy(array, 2n, (x) => x)).toEqual(2);
+    expect(sortedLastIndexBy(array, 3n, (x) => x)).toEqual(3);
+    expect(sortedLastIndexBy(array, 4n, (x) => x)).toEqual(3);
+  });
+
+  it("handles a sparse array", () => {
+    const array: [bigint, bigint][] = [
+      [1n, 42n],
+      [3n, 42n],
+    ];
+
+    expect(sortedLastIndexBy(array, 0n, (x) => x)).toEqual(0);
+    expect(sortedLastIndexBy(array, 1n, (x) => x)).toEqual(1);
+    expect(sortedLastIndexBy(array, 2n, (x) => x)).toEqual(1);
+    expect(sortedLastIndexBy(array, 3n, (x) => x)).toEqual(2);
+    expect(sortedLastIndexBy(array, 4n, (x) => x)).toEqual(2);
+  });
+
+  it("handles negation", () => {
+    const array: [bigint, bigint][] = [
+      [1n, 42n],
+      [2n, 42n],
+      [3n, 42n],
+      [4n, 42n],
+    ];
+
+    expect(sortedLastIndexBy(array, 0n, (x) => -x)).toEqual(4);
+    expect(sortedLastIndexBy(array, 1n, (x) => -x)).toEqual(4);
+    expect(sortedLastIndexBy(array, 2n, (x) => -x)).toEqual(4);
+    expect(sortedLastIndexBy(array, 3n, (x) => -x)).toEqual(4);
+    expect(sortedLastIndexBy(array, 4n, (x) => -x)).toEqual(0);
+    expect(sortedLastIndexBy(array, 5n, (x) => -x)).toEqual(0);
+  });
+
+  it("handles a contiguous array with duplicate times", () => {
+    const array: [bigint, bigint][] = [
+      [1n, 42n],
+      [2n, 42n],
+      [2n, 42n],
+      [2n, 42n],
+      [3n, 42n],
+      [3n, 42n],
+    ];
+
+    expect(sortedLastIndexBy(array, 0n, (x) => x)).toEqual(0);
+    expect(sortedLastIndexBy(array, 1n, (x) => x)).toEqual(1);
+    expect(sortedLastIndexBy(array, 2n, (x) => x)).toEqual(4);
+    expect(sortedLastIndexBy(array, 3n, (x) => x)).toEqual(6);
+  });
+});
