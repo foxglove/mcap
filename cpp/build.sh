9e47930be465c01929f562b3495a5913651bf1fc
Filippo Brizzi <filippobrizzi@gmail.com>
2024-02-26 13:06:33 +0000

    test are run via cmake

--- cpp/build.sh
+++ cpp/build.sh
@@ -5,8 +5,6 @@ set -e
 conan config init
 
 conan editable add ./mcap mcap/1.4.0
-conan install test --install-folder test/build/Debug \
-  -s compiler.cppstd=17 -s build_type=Debug --build missing
 
 if [ "$1" != "--build-tests-only" ]; then
   conan install bench --install-folder bench/build/Release \
@@ -17,4 +15,5 @@ if [ "$1" != "--build-tests-only" ]; then
   conan build bench --build-folder bench/build/Release
 fi
 
-conan build test --build-folder test/build/Debug
+cmake . -B ./build -DCMAKE_BUILD_TYPE=Debug
+cmake --build ./build
