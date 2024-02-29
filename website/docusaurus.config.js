2922877fd879baea92c9e9db01f61aba82e5a4f8
James Smith <james@foxglove.dev>
2024-02-29 14:31:05 +1100

    use abs path of git

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -70,7 +70,7 @@ const config = {
         async loadContent() {
           /* cspell:disable */
           const result = await execAsync(
-            `git tag --sort=-creatordate --list "releases/mcap-cli/*"`,
+            `/usr/bin/git tag --sort=-creatordate --list "releases/mcap-cli/*"`,
           );
           /* cspell:enable */
           if (result.stdout.length === 0) {
