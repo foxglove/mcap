f6846ee01597ece5311ed9f463ac328c7125bcc5
Brad Squicciarini <brad@cocodelivery.com>
2024-03-29 11:44:35 -0700

    remove deprecated MessageFactory

--- python/mcap-protobuf-support/mcap_protobuf/decoder.py
+++ python/mcap-protobuf-support/mcap_protobuf/decoder.py
@@ -1,9 +1,10 @@
 import warnings
 from collections import Counter
-from typing import Any, Callable, Dict, Iterable, Optional, Type
+from typing import Any, Callable, Dict, Optional, Type
 
-from google.protobuf.descriptor_pb2 import FileDescriptorProto, FileDescriptorSet
-from google.protobuf.message_factory import MessageFactory
+from google.protobuf.descriptor_pb2 import FileDescriptorSet
+from google.protobuf.descriptor_pool import DescriptorPool
+from google.protobuf.message_factory import GetMessageClassesForFiles
 
 from mcap.decoder import DecoderFactory as McapDecoderFactory
 from mcap.exceptions import McapError
@@ -25,63 +26,6 @@ class DecoderFactory(McapDecoderFactory):
     def __init__(self):
         self._types: Dict[int, Type[Any]] = {}
 
-    def _get_message_classes(self, file_descriptors: Iterable[FileDescriptorProto]):
-        """Adds file descriptors to the message factory pool in topological order, then returns
-        the message classes for all file descriptors.
-
-        Modified from the original at:
-        https://github.com/protocolbuffers/protobuf/blob/main/python/google/protobuf/message_factory.py
-
-        Protocol Buffers - Google's data interchange format
-        Copyright 2008 Google Inc.  All rights reserved.
-        https://developers.google.com/protocol-buffers/
-
-        Redistribution and use in source and binary forms, with or without
-        modification, are permitted provided that the following conditions are
-        met:
-
-            * Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
-            * Redistributions in binary form must reproduce the above
-        copyright notice, this list of conditions and the following disclaimer
-        in the documentation and/or other materials provided with the
-        distribution.
-            * Neither the name of Google Inc. nor the names of its
-        contributors may be used to endorse or promote products derived from
-        this software without specific prior written permission.
-
-        THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-        "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-        LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-        A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
-        OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
-        SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
-        LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
-        DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
-        THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-        (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-        OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-        """
-        descriptor_by_name = {
-            file_descriptor.name: file_descriptor
-            for file_descriptor in file_descriptors
-        }
-        factory = MessageFactory()
-
-        def _add_file(file_descriptor: FileDescriptorProto):
-            for dependency in file_descriptor.dependency:
-                if dependency in descriptor_by_name:
-                    # Remove from elements to be visited, in order to cut cycles.
-                    _add_file(descriptor_by_name.pop(dependency))
-            factory.pool.Add(file_descriptor)
-
-        while descriptor_by_name:
-            _add_file(descriptor_by_name.popitem()[1])
-
-        return factory.GetMessages(
-            [file_descriptor.name for file_descriptor in file_descriptors]
-        )
-
     def decoder_for(
         self, message_encoding: str, schema: Optional[Schema]
     ) -> Optional[Callable[[bytes], Any]]:
@@ -100,7 +44,12 @@ class DecoderFactory(McapDecoderFactory):
                     raise McapError(
                         f"FileDescriptorSet contains {count} file descriptors for {name}"
                     )
-            messages = self._get_message_classes(fds.file)
+
+            pool = DescriptorPool()
+            for fd in fds.file:
+                pool.Add(fd)
+            messages = GetMessageClassesForFiles([fd.name for fd in fds.file], pool)
+
             for name, klass in messages.items():
                 if name == schema.name:
                     self._types[schema.id] = klass
