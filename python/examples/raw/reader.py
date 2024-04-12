7bfa9f8a30427742cb4595dec844e549b4a764ee
Chris Ayling <chris.ayling.360@gmail.com>
2024-04-12 16:37:50 +1000

    fix python example raw reader topic to match writer

--- python/examples/raw/reader.py
+++ python/examples/raw/reader.py
@@ -4,5 +4,5 @@ from mcap.reader import make_reader
 
 with open(sys.argv[1], "rb") as f:
     reader = make_reader(f)
-    for schema, channel, message in reader.iter_messages(topics=["/diagnostics"]):
+    for schema, channel, message in reader.iter_messages(topics=["sample_topic"]):
         print(f"{channel.topic} ({schema.name}): {message.data}")
