bfcbdfe9e4a09eac3bf35ff1c97e021021291e84
James Smith <james@foxglove.dev>
2024-05-08 15:11:05 +1000

    remove duplicate channel lookup

--- go/mcap/unindexed_message_iterator.go
+++ go/mcap/unindexed_message_iterator.go
@@ -60,7 +60,8 @@ func (it *unindexedMessageIterator) NextInto(msg *Message) (*Schema, *Channel, e
 				return nil, nil, err
 			}
 			msg.Data = append(existingbuf[:0], msg.Data...)
-			if _, ok := it.channels[msg.ChannelID]; !ok {
+			channel, ok := it.channels[msg.ChannelID]
+			if !ok {
 				// skip messages on channels we don't know about. Note that if
 				// an unindexed reader encounters a message it would be
 				// interested in, but has not yet encountered the corresponding
@@ -68,10 +69,6 @@ func (it *unindexedMessageIterator) NextInto(msg *Message) (*Schema, *Channel, e
 				continue
 			}
 			if msg.LogTime >= it.start && msg.LogTime < it.end {
-				channel, ok := it.channels[msg.ChannelID]
-				if !ok {
-					return nil, nil, fmt.Errorf("message with unrecognized channel ID %d", msg.ChannelID)
-				}
 				schema, ok := it.schemas[channel.SchemaID]
 				if !ok && channel.SchemaID != 0 {
 					return nil, nil, fmt.Errorf("channel %d with unrecognized schema ID %d", msg.ChannelID, channel.SchemaID)
