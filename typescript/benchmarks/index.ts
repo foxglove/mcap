88db9982cc9ed38a3230cd76929ebab02cdb0163
James Smith <james@foxglove.dev>
2024-06-20 12:14:09 +1000

    add benchmark that I like

--- typescript/benchmarks/index.ts
+++ typescript/benchmarks/index.ts
@@ -6,7 +6,9 @@ import {
   FastIndexedReader,
 } from "@mcap/core";
 import assert from "assert";
-import { add, complete, cycle, suite } from "benny";
+import { program } from "commander";
+
+import { runBenchmark } from "./bench";
 
 /**
  * An IWritable that copies data to memory, but overwrites previous data. This allows benchmarking
@@ -37,20 +39,6 @@ class FakeMemoryWritable {
   }
 }
 
-export async function benchmarkWriter(): Promise<void> {
-  await suite(
-    McapWriter.name,
-    addWriteBenchmark({ numMessages: 1_000_000, messageSize: 1, chunkSize: 1024 * 1024 }),
-    addWriteBenchmark({ numMessages: 100_000, messageSize: 1000, chunkSize: 1024 * 1024 }),
-    addWriteBenchmark({ numMessages: 100, messageSize: 1_000_000, chunkSize: 1024 * 1024 }),
-    addWriteBenchmark({ numMessages: 1_000_000, messageSize: 1, chunkSize: 10 * 1024 * 1024 }),
-    addWriteBenchmark({ numMessages: 100_000, messageSize: 1000, chunkSize: 10 * 1024 * 1024 }),
-    addWriteBenchmark({ numMessages: 100, messageSize: 1_000_000, chunkSize: 10 * 1024 * 1024 }),
-    cycle(),
-    complete(),
-  );
-}
-
 async function benchmarkReaders() {
   const messageSize = 10;
   const chunkSize = 1024 * 1024 * 4;
@@ -78,62 +66,70 @@ async function benchmarkReaders() {
     return `${name} (1 op ≈ ${(numMessages * messageSize).toLocaleString()} bytes)`;
   };
   await writer.end();
-  await suite(
-    "reader",
-    add(description(McapStreamReader.name), async () => {
-      const reader = new McapStreamReader();
-      reader.append(buf.get());
-      let messageCount = 0;
-      for (;;) {
-        const rec = reader.nextRecord();
-        if (rec != undefined) {
-          if (rec.type === "Message") {
-            messageCount++;
-          }
-        } else {
-          break;
+  await runBenchmark(description(McapStreamReader.name), async () => {
+    const reader = new McapStreamReader();
+    reader.append(buf.get());
+    let messageCount = 0;
+    for (;;) {
+      const rec = reader.nextRecord();
+      if (rec != undefined) {
+        if (rec.type === "Message") {
+          messageCount++;
         }
+      } else {
+        break;
       }
-      assert(messageCount === numMessages, `expected ${numMessages} messages, got ${messageCount}`);
-    }),
-    add(description(FastIndexedReader.name), async () => {
-      const reader = await FastIndexedReader.Initialize({ readable: buf });
-      let messageCount = 0;
-      for await (const _ of reader.readMessages({ reverse: false })) {
-        messageCount++;
-      }
-      assert(messageCount === numMessages, `expected ${numMessages} messages, got ${messageCount}`);
-    }),
-    add(description(FastIndexedReader.name + "_reverse"), async () => {
-      const reader = await FastIndexedReader.Initialize({ readable: buf });
-      let messageCount = 0;
-      for await (const _ of reader.readMessages({ reverse: true })) {
-        messageCount++;
-      }
-      assert(messageCount === numMessages, `expected ${numMessages} messages, got ${messageCount}`);
-    }),
-    add(description(McapIndexedReader.name), async () => {
-      const reader = await McapIndexedReader.Initialize({ readable: buf });
-      let messageCount = 0;
-      for await (const _ of reader.readMessages()) {
-        messageCount++;
-      }
-      assert(messageCount === numMessages, `expected ${numMessages} messages, got ${messageCount}`);
-    }),
-    add(description(McapIndexedReader.name + "_reverse"), async () => {
-      const reader = await McapIndexedReader.Initialize({ readable: buf });
-      let messageCount = 0;
-      for await (const _ of reader.readMessages({ reverse: true })) {
-        messageCount++;
-      }
-      assert(messageCount === numMessages, `expected ${numMessages} messages, got ${messageCount}`);
-    }),
-    cycle(),
-    complete(),
-  );
+    }
+    assert(messageCount === numMessages, `expected ${numMessages} messages, got ${messageCount}`);
+  });
+  await runBenchmark(description(FastIndexedReader.name), async () => {
+    const reader = await FastIndexedReader.Initialize({ readable: buf });
+    let messageCount = 0;
+    for await (const _ of reader.readMessages({ reverse: false })) {
+      messageCount++;
+    }
+    assert(messageCount === numMessages, `expected ${numMessages} messages, got ${messageCount}`);
+  });
+  await runBenchmark(description(FastIndexedReader.name + "_reverse"), async () => {
+    const reader = await FastIndexedReader.Initialize({ readable: buf });
+    let messageCount = 0;
+    for await (const _ of reader.readMessages({ reverse: true })) {
+      messageCount++;
+    }
+    assert(messageCount === numMessages, `expected ${numMessages} messages, got ${messageCount}`);
+  });
+  await runBenchmark(description(McapIndexedReader.name), async () => {
+    const reader = await McapIndexedReader.Initialize({ readable: buf });
+    let messageCount = 0;
+    for await (const _ of reader.readMessages()) {
+      messageCount++;
+    }
+    assert(messageCount === numMessages, `expected ${numMessages} messages, got ${messageCount}`);
+  });
+  await runBenchmark(description(McapIndexedReader.name + "_reverse"), async () => {
+    const reader = await McapIndexedReader.Initialize({ readable: buf });
+    let messageCount = 0;
+    for await (const _ of reader.readMessages({ reverse: true })) {
+      messageCount++;
+    }
+    assert(messageCount === numMessages, `expected ${numMessages} messages, got ${messageCount}`);
+  });
 }
 
-function addWriteBenchmark({
+export async function benchmarkWriter(): Promise<void> {
+  await runWriteBenchmark({ numMessages: 1_000_000, messageSize: 1, chunkSize: 1024 * 1024 });
+  await runWriteBenchmark({ numMessages: 100_000, messageSize: 1000, chunkSize: 1024 * 1024 });
+  await runWriteBenchmark({ numMessages: 100, messageSize: 1_000_000, chunkSize: 1024 * 1024 });
+  await runWriteBenchmark({ numMessages: 1_000_000, messageSize: 1, chunkSize: 10 * 1024 * 1024 });
+  await runWriteBenchmark({ numMessages: 100_000, messageSize: 1000, chunkSize: 10 * 1024 * 1024 });
+  await runWriteBenchmark({
+    numMessages: 100,
+    messageSize: 1_000_000,
+    chunkSize: 10 * 1024 * 1024,
+  });
+}
+
+async function runWriteBenchmark({
   numMessages,
   messageSize,
   chunkSize,
@@ -142,41 +138,49 @@ function addWriteBenchmark({
   messageSize: number;
   chunkSize: number;
 }) {
-  return add(
+  const messageData = new Uint8Array(messageSize).fill(42);
+  const writable = new FakeMemoryWritable(2 * chunkSize);
+  await runBenchmark(
     `count=${numMessages.toLocaleString()} size=${messageSize.toLocaleString()} chunkSize=${chunkSize.toLocaleString()} (1 op ≈ ${(
       numMessages * messageSize
     ).toLocaleString()} bytes)`,
     async () => {
-      const messageData = new Uint8Array(messageSize).fill(42);
-      const writable = new FakeMemoryWritable(2 * chunkSize);
-      return async () => {
-        writable.reset();
-        const writer = new McapWriter({ writable, chunkSize });
-        await writer.start({ library: "", profile: "" });
-        const channelId = await writer.registerChannel({
-          schemaId: 0,
-          topic: "",
-          messageEncoding: "",
-          metadata: new Map([]),
+      writable.reset();
+      const writer = new McapWriter({ writable, chunkSize });
+      await writer.start({ library: "", profile: "" });
+      const channelId = await writer.registerChannel({
+        schemaId: 0,
+        topic: "",
+        messageEncoding: "",
+        metadata: new Map([]),
+      });
+      for (let i = 0; i < numMessages; i++) {
+        await writer.addMessage({
+          channelId,
+          sequence: i,
+          logTime: BigInt(i),
+          publishTime: BigInt(i),
+          data: messageData,
         });
-        for (let i = 0; i < numMessages; i++) {
-          await writer.addMessage({
-            channelId,
-            sequence: i,
-            logTime: BigInt(i),
-            publishTime: BigInt(i),
-            data: messageData,
-          });
-        }
-        await writer.end();
-      };
+      }
+      await writer.end();
     },
   );
 }
 
-async function main() {
-  // await benchmarkWriter();
-  await benchmarkReaders();
+async function main(args: { suite?: string }) {
+  const { suite } = args;
+  if (suite == undefined || suite === "writer") {
+    console.log("Running 'writer' suite");
+    await benchmarkWriter();
+  }
+  if (suite == undefined || suite === "reader") {
+    console.log("Running 'reader' suite");
+    await benchmarkReaders();
+  }
 }
 
-void main();
+program
+  .addOption(program.createOption("--suite <suite>", "Name of suite to run"))
+  .action(main)
+  .parse();
