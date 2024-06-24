2da6cf1884228fa1ed22e1e4f71b183845dad80c
Jacob Bandes-Storch <jacob@foxglove.dev>
2024-06-24 12:28:33 -0700

    nitty nit

--- typescript/examples/validate/scripts/validate.ts
+++ typescript/examples/validate/scripts/validate.ts
@@ -139,7 +139,7 @@ async function validate(
           throw new Error(`Missing schema ${record.schemaId} for channel ${record.id}`);
         }
         let messageDeserializer: (data: ArrayBufferView) => unknown;
-        if (record.messageEncoding === "ros1" && schema.encoding === "ros1msg") {
+        if (schema.encoding === "ros1msg" && record.messageEncoding === "ros1") {
           const reader = new ROS1LazyMessageReader(
             parseMessageDefinition(new TextDecoder().decode(schema.data)),
           );
@@ -150,14 +150,14 @@ async function validate(
             }
             return reader.readMessage(data).toJSON();
           };
-        } else if (record.messageEncoding === "cdr" && schema.encoding === "ros2msg") {
+        } else if (schema.encoding === "ros2msg" && record.messageEncoding === "cdr") {
           const reader = new ROS2MessageReader(
             parseMessageDefinition(new TextDecoder().decode(schema.data), {
               ros2: true,
             }),
           );
           messageDeserializer = (data) => reader.readMessage(data);
-        } else if (record.messageEncoding === "protobuf" && schema.encoding === "protobuf") {
+        } else if (schema.encoding === "protobuf" && record.messageEncoding === "protobuf") {
           const root = protobufjs.Root.fromDescriptor(FileDescriptorSet.decode(schema.data));
           const type = root.lookupType(schema.name);
 
