69ba094da947e1230819fe2888922b89de35d0b3
foxglove-bw <177747810+foxglove-bw@users.noreply.github.com>
2024-08-18 09:10:06 +1000

    Add info snapshot test

--- rust/cli/src/tests/e2e_info.rs
+++ rust/cli/src/tests/e2e_info.rs
@@ -1,4 +1,7 @@
 use assert_cmd::Command;
+use predicates::str::diff;
+
+use crate::tests::common::demo_file;
 
 use super::common::conformance_mcap_files;
 
@@ -9,4 +12,17 @@ async fn test_info_passes_conformance_tests() {
 
         cmd.args(["info", &file]).assert().success();
     }
+}
+
+#[tokio::test]
+async fn test_info_demo_file_snapshot() {
+    let file = demo_file();
+
+    let mut cmd = Command::cargo_bin("cli").expect("failed to create command");
+
+    cmd.args(["info", &file])
+        .assert()
+        .success()
+        // re-create this with `cargo run -p cli -- info <demo file>`
+        .stdout(diff(include_str!("./data/e2e_info_demo_file.stdout")));
 }
