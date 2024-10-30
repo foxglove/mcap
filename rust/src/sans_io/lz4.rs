c3f39b91d20fc344f9cedcae461f4e8b15b90743
james-rms <james@foxglove.dev>
2024-10-30 11:37:02 +1100

    rust: add sans-io linear reader (#1244)
    ### Changelog
    - Rust: Added a `sans_io` module to allow users to read MCAP data while
    remaining agnostic to I/O. Replace the internal implementation details
    of both the memory-mapped I/O and tokio-async read modules with this
    sans_io reader.
    
    ### Docs
    
    See generated rust docs.
    
    ### Description
    
    As we need more functionality from the async MCAP reader, I feel less
    comfortable adding those and not also ensuring they're available for the
    memory-mapped reader.
    
    This PR strips out all of that code and moves it into a
    [sans-io](https://sans-io.readthedocs.io/) reader module. The sans-io
    reader is a state machine with no dependency on any particular I/O
    framework.
    <!-- In addition to unit tests, describe any manual testing you did to
    validate this change. -->
    
    <table><tr><th>Before</th><th>After</th></tr><tr><td>
    
    <!--before content goes here-->
    
    </td><td>
    
    <!--after content goes here-->
    
    </td></tr></table>
    
    <!-- If necessary, link relevant Linear or Github issues. Use `Fixes:
    foxglove/repo#1234` to auto-close the Github issue or Fixes: FG-### for
    Linear isses. -->
    
    ---------
    
    Co-authored-by: Jacob Bandes-Storch <jacob@foxglove.dev>

---
+++ rust/src/sans_io/lz4.rs
@@ -0,0 +1,67 @@
+use crate::sans_io::decompressor::{DecompressResult, Decompressor};
+use crate::McapResult;
+use std::ptr;
+
+use lz4::liblz4::{
+    check_error, LZ4FDecompressionContext, LZ4F_createDecompressionContext, LZ4F_decompress,
+    LZ4F_freeDecompressionContext, LZ4F_resetDecompressionContext, LZ4F_VERSION,
+};
+
+/// A Decompressor wrapper for LZ4 streaming decompression.
+#[derive(Debug)]
+pub struct Lz4Decoder {
+    c: LZ4FDecompressionContext,
+    next_read_size: usize,
+}
+
+impl Lz4Decoder {
+    pub fn new() -> McapResult<Lz4Decoder> {
+        let mut context = LZ4FDecompressionContext(ptr::null_mut());
+        check_error(unsafe { LZ4F_createDecompressionContext(&mut context, LZ4F_VERSION) })?;
+        Ok(Lz4Decoder {
+            c: context,
+            next_read_size: 13, // min frame size
+        })
+    }
+}
+
+impl Drop for Lz4Decoder {
+    fn drop(&mut self) {
+        unsafe { LZ4F_freeDecompressionContext(self.c) };
+    }
+}
+
+impl Decompressor for Lz4Decoder {
+    fn next_read_size(&self) -> usize {
+        self.next_read_size
+    }
+
+    fn decompress(&mut self, src: &[u8], dst: &mut [u8]) -> McapResult<DecompressResult> {
+        let mut dst_size = dst.len();
+        let mut src_size = src.len();
+        let need = check_error(unsafe {
+            LZ4F_decompress(
+                self.c,
+                dst.as_mut_ptr(),
+                &mut dst_size,
+                src.as_ptr(),
+                &mut src_size,
+                ptr::null(),
+            )
+        })?;
+        self.next_read_size = need;
+        Ok(DecompressResult {
+            consumed: src_size,
+            wrote: dst_size,
+        })
+    }
+
+    fn reset(&mut self) -> McapResult<()> {
+        unsafe { LZ4F_resetDecompressionContext(self.c) };
+        Ok(())
+    }
+
+    fn name(&self) -> &'static str {
+        "lz4"
+    }
+}
