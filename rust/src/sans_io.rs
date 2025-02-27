b27611a2fc8f67b27e671eef029db5c40fc4264a
James Smith <james@foxglove.dev>
2025-02-27 12:56:48 +1100

    rust: rename items in linear reader

--- rust/src/sans_io.rs
+++ rust/src/sans_io.rs
@@ -1,6 +1,8 @@
 //! Read MCAP files from any source of bytes
 pub mod decompressor;
-pub mod read;
+pub mod linear_reader;
+
+pub use linear_reader::{LinearReadEvent, LinearReader, LinearReaderOptions};
 
 #[cfg(feature = "lz4")]
 mod lz4;
