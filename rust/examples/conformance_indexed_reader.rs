f62c21c98d83061317b5f9f2aff172944e53cca9
James Smith <james@foxglove.dev>
2025-03-06 11:56:44 -0700

    simplify conformance reader

--- rust/examples/conformance_indexed_reader.rs
+++ rust/examples/conformance_indexed_reader.rs
@@ -3,6 +3,7 @@ mod serialization;
 use std::collections::BTreeSet;
 use std::io::{Cursor, Read, Seek};
 
+use mcap::records::SchemaHeader;
 use serde_json::{json, Value};
 
 use std::env;
@@ -16,9 +17,6 @@ pub fn main() {
     }
     let file = std::fs::read(&args[1]).expect("file wouldn't open");
     let mut messages: Vec<Value> = vec![];
-    let mut schemas: Vec<Value> = vec![];
-    let mut channels: Vec<Value> = vec![];
-    let mut statistics: Vec<Value> = vec![];
     let mut cursor = Cursor::new(&file);
     let summary = {
         let mut reader = mcap::sans_io::SummaryReader::new();
@@ -35,8 +33,7 @@ pub fn main() {
         }
         reader.finish().expect("file should have a summary section")
     };
-    let mut seen_schemas = BTreeSet::new();
-    let mut seen_channels = BTreeSet::new();
+
     let mut reader =
         mcap::sans_io::IndexedReader::new(&summary).expect("failed to initialize indexed reader");
     while let Some(event) = reader.next_event() {
@@ -49,36 +46,6 @@ pub fn main() {
                 reader.notify_read(read);
             }
             mcap::sans_io::IndexedReadEvent::Message { header, data } => {
-                let channel_id = header.channel_id;
-                if !seen_channels.contains(&channel_id) {
-                    let channel = summary
-                        .channels
-                        .get(&channel_id)
-                        .expect("malformed MCAP: channel not in summary");
-                    if let Some(schema) = channel.schema.clone() {
-                        if !seen_schemas.contains(&schema.id) {
-                            schemas.push(serialization::as_json(&mcap::records::Record::Schema {
-                                header: mcap::records::SchemaHeader {
-                                    id: schema.id,
-                                    name: schema.name.clone(),
-                                    encoding: schema.encoding.clone(),
-                                },
-                                data: schema.data.clone(),
-                            }));
-                            seen_schemas.insert(schema.id);
-                        }
-                    }
-                    channels.push(serialization::as_json(&mcap::records::Record::Channel(
-                        mcap::records::Channel {
-                            id: channel.id,
-                            schema_id: channel.schema.as_ref().map(|s| s.id).unwrap_or(0),
-                            topic: channel.topic.clone(),
-                            message_encoding: channel.message_encoding.clone(),
-                            metadata: channel.metadata.clone(),
-                        },
-                    )));
-                    seen_channels.insert(channel.id);
-                }
                 messages.push(serialization::as_json(&mcap::records::Record::Message {
                     header,
                     data: std::borrow::Cow::Borrowed(data),
@@ -87,11 +54,41 @@ pub fn main() {
         }
     }
 
+    let mut statistics: Vec<Value> = vec![];
     if let Some(stats) = summary.stats {
         statistics.push(serialization::as_json(&mcap::records::Record::Statistics(
             stats.clone(),
         )));
-    }
+    };
+
+    let schemas: Vec<_> = summary
+        .schemas
+        .values()
+        .map(|schema| {
+            serialization::as_json(&mcap::records::Record::Schema {
+                header: SchemaHeader {
+                    id: schema.id,
+                    name: schema.name.clone(),
+                    encoding: schema.encoding.clone(),
+                },
+                data: schema.data.to_owned(),
+            })
+        })
+        .collect();
+
+    let channels: Vec<_> = summary
+        .channels
+        .values()
+        .map(|channel| {
+            serialization::as_json(&mcap::records::Record::Channel(mcap::records::Channel {
+                id: channel.id,
+                schema_id: channel.schema.as_ref().map(|s| s.id).unwrap_or(0),
+                topic: channel.topic.clone(),
+                message_encoding: channel.message_encoding.clone(),
+                metadata: channel.metadata.clone(),
+            }))
+        })
+        .collect();
 
     let out = json!({ "messages": messages, "schemas": schemas, "channels": channels, "statistics": statistics });
     print!("{}", serde_json::to_string_pretty(&out).unwrap());
