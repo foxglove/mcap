f12f344182c60a6fb049cb8b95820549a60928eb
Adrian Macneil <adrian@foxglove.dev>
2025-05-17 22:17:53 -0700

    sharpen larger images too

---
+++ website/static/img/generate_thumbs.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eux
+
+cd "$(dirname "$0")"
+magick -background none mcap.svg -resize 36x36 -gravity center -extent 32x32 -sharpen 0x1.0 favicon.png
+magick -background none mcap.svg -resize 240x -sharpen 0x0.5 mcap240.webp
+magick -background none mcap.svg -resize 720x -sharpen 0x0.5 mcap720.webp
+ls -lh
--- website/static/img/genthumbs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eux
-
-magick -background none mcap.svg -resize 36x36 -gravity center -extent 32x32 -sharpen 0x1.0 favicon.png
-magick -background none mcap.svg -resize 240x mcap240.webp
-magick -background none mcap.svg -resize 720x mcap720.webp
-ls -lh
