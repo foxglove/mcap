8810067675c0f6eb00d6d9dd45a80231da447ada
Jacob Bandes-Storch <jacob@foxglove.dev>
2025-10-07 22:50:40 -0700

    cpp: bump version to 2.1.1 (#1466)
    ### Changelog
    - #1465

--- cpp/build-docs.sh
+++ cpp/build-docs.sh
@@ -4,7 +4,7 @@ set -e
 
 conan config init
 
-conan editable add ./mcap mcap/2.1.0
+conan editable add ./mcap mcap/2.1.1
 conan install docs --install-folder docs/build/Release \
   -s compiler.cppstd=17 -s build_type=Release --build missing
 
--- cpp/test/conanfile.py
+++ cpp/test/conanfile.py
@@ -4,7 +4,7 @@ from conans import ConanFile, CMake
 class McapTestConan(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
     generators = "cmake"
-    requires = "catch2/2.13.8", "mcap/2.1.0", "nlohmann_json/3.10.5"
+    requires = "catch2/2.13.8", "mcap/2.1.1", "nlohmann_json/3.10.5"
 
     def build(self):
         cmake = CMake(self)
