e0c5b6356e158d9a0952055336df9befc3c05cd0
Andreas Ringlstetter <andreas.ringlstetter@digitalwerk.net>
2026-02-03 13:32:40 +0100

    CMake downgrade

--- cpp/test_package/test_odr.cpp
+++ cpp/test_package/test_odr.cpp
@@ -1,4 +1,3 @@
 // Check that all modules support usage from multiple translation units.
-#include <mcap/crc32.hpp>
-#include <mcap/internal.hpp>
 #include <mcap/mcap.hpp>
+#include <mcap/crc32.hpp>
