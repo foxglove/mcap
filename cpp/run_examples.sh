9e0b04235690199260a0ebf7ada1452c74368a92
Jacob Bandes-Storch <jacob@foxglove.dev>
2026-02-04 13:20:37 -0700

    more path fixes

--- cpp/run_examples.sh
+++ cpp/run_examples.sh
@@ -4,6 +4,6 @@ set -e
 
 proto_mcap_filepath=/tmp/example_out.mcap
 
-examples/build/Release/example_protobuf_writer $proto_mcap_filepath
-examples/build/Release/example_protobuf_static_reader $proto_mcap_filepath
-examples/build/Release/example_protobuf_dynamic_reader $proto_mcap_filepath
+examples/build/Release/protobuf/example_protobuf_writer $proto_mcap_filepath
+examples/build/Release/protobuf/example_protobuf_static_reader $proto_mcap_filepath
+examples/build/Release/protobuf/example_protobuf_dynamic_reader $proto_mcap_filepath
